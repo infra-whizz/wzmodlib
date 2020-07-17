3ac3d3d175afcd5b334cbc7ef687906e6a3b47e0
Bo Maryniuk <bo@maryniuk.net>
2020-07-17 20:22:50 +0200

    Add ansible value converter util functions

--- stringutils.go
+++ stringutils.go
@@ -2,7 +2,7 @@ package wzmodlib
 
 import "strings"
 
-//
+// Byte65toS converts null terminated string of 65 bytes into a string
 func Byte65toS(cstring [65]byte) string {
 	buf := make([]byte, len(cstring))
 	for i, b := range cstring {
@@ -13,4 +13,26 @@ func Byte65toS(cstring [65]byte) string {
 		str = str[:i]
 	}
 	return str
+}
+
+// YesNo2Bool converts Ansible's "yes" and "no" too true and false respectively
+func YesNo2Bool(val string) bool {
+	if val != "" {
+		if strings.ToLower(val) == "yes" {
+			return true
+		} else {
+			return false
+		}
+	} else {
+		return false
+	}
+}
+
+// Bool2Int converts true/false to 1/0 respectively
+func Bool2Int(val bool) int {
+	if val {
+		return 1
+	} else {
+		return 0
+	}
 }
