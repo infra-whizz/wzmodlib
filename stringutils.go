7bd7a478413faa9760e4711189d8e39ae90c7fc7
Bo Maryniuk <bo@maryniuk.net>
2020-07-20 17:15:32 +0200

    Add more utils for handling Ansible arguments

--- stringutils.go
+++ stringutils.go
@@ -1,6 +1,9 @@
 package wzmodlib
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 // Byte65toS converts null terminated string of 65 bytes into a string
 func Byte65toS(cstring [65]byte) string {
@@ -35,4 +38,37 @@ func Bool2Int(val bool) int {
 	} else {
 		return 0
 	}
+}
+
+// SInList checks if a string is a list of strings
+func SInList(val string, list []string) bool {
+	for _, sval := range list {
+		if val == sval {
+			return true
+		}
+	}
+	return false
+}
+
+// CheckAnsibleParameter if option with the value meets the expectations
+func CheckAnsibleParameter(opt string, val string, expected []string) error {
+	if !SInList(strings.ToLower(val), expected) {
+		return fmt.Errorf("Unknown value for '%s': %s. Expected: %s", opt, val, strings.Join(expected, ", "))
+	}
+	return nil
+}
+
+// CheckAnsibleBool checks if the value is within "yes" or "no" and if it is empty, then default value is set
+func CheckAnsibleBool(opt string, val string, on bool) (string, error) {
+	if val == "" {
+		if on {
+			val = "yes"
+		} else {
+			val = "no"
+		}
+	} else if err := CheckAnsibleParameter(opt, val, []string{"yes", "no"}); err != nil {
+		return "", err
+	}
+
+	return val, nil
 }
